f6031bb66e2d0be0a97fa7c76b521b8980a0f129
turkins <kirils.turkins@adesso.de>
2026-02-12 18:11:33 +0100

    Added missing dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,10 +19,16 @@ java {
 
 val javaLinVersion = "6.7.0"
 val jakartaMailVersion = "2.1.5"
+val jacksonVersion = "2.21.0"
+val simpleSlf4jVersion = "2.0.17"
 
 dependencies {
     implementation("io.javalin:javalin:$javaLinVersion")
     implementation("jakarta.mail:jakarta.mail-api:$jakartaMailVersion")
+    implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+
+    runtimeOnly("org.slf4j:slf4j-simple:$simpleSlf4jVersion")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
